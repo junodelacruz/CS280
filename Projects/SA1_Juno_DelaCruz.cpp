423b0c10800c01fd6b9f01f6bb7b066164caf271
Anetryst <juno.delacruz0124@gmail.com>
2025-01-28 18:39:27 -0500

    input fix

--- Projects/SA1_Juno_DelaCruz.cpp
+++ Projects/SA1_Juno_DelaCruz.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(void){
     string input = "";
-    cout << "Welcome to CS280 (Spring 2025) \nWhat is your first name, last name, and section number?" << endl;
+    cout << "Welcome to CS 280 (Spring 2025)\nWhat are your first name, last name, and section number?" << endl;
     getline(cin, input);
     string section;
     for(int i = 0; i<input.length(); i++){
@@ -12,6 +12,6 @@ int main(void){
         }
     }
     string firstname = input.substr(0, input.find(" "));
-    cout << "Hello " + firstname + ", Welcome to CS 280 Section " + section + ".";
+    cout << "Hello " + firstname + ", Welcome to CS 280 Section " + section + ".\n";
 }
 
