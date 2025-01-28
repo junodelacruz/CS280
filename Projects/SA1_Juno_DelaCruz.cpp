05541e77cf2e7aab3633004623c233462de077a1
anetryst <juno.delacruz0124@gmail.com>
2025-01-28 12:53:18 -0500

    SA1 done

--- Projects/SA1_Juno_DelaCruz.cpp
+++ Projects/SA1_Juno_DelaCruz.cpp
@@ -4,9 +4,14 @@ using namespace std;
 int main(void){
     string input = "";
     cout << "Welcome to CS280 (Spring 2025) \nWhat is your first name, last name, and section number?" << endl;
-    cin >> input;
-    char section = input.at(input.length() - 1);
-    cout << section;
-    cout << "Hello " + input + ", Welcome to CS 280 Section ";
+    getline(cin, input);
+    string section;
+    for(int i = 0; i<input.length(); i++){
+        if(isdigit(input[i])){
+            section += input[i];
+        }
+    }
+    string firstname = input.substr(0, input.find(" "));
+    cout << "Hello " + firstname + ", Welcome to CS 280 Section " + section;
 }
 
