772a04c5446cd2ac9c3f7a060619b869b0307040
anetryst <juno.delacruz0124@gmail.com>
2025-03-31 10:08:12 -0400

    lecture notes chapter 10 [2025-3-31 Mon]

---
+++ Notes/WeekTen/WeekTen1.cpp
@@ -0,0 +1,16 @@
+// Lecture 16 and 17 Data Types [2025-3-27 Thu]
+
+// computers dont subtract, they just add negatives
+
+// Lecture 16 and 17 Data Types [2025-3-31 Mon]
+
+// read the slides, no real programming during the lectures
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    return 0;
+}
